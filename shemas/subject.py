cd44f531d761b049d2b8c9ac514e183fcf78e47e
megan <julie.meh@yandex.ru>
2023-10-08 22:30:52 +0300

    feat: added shemas

---
+++ shemas/subject.py
@@ -0,0 +1,27 @@
+from datetime import time
+
+from pydantic import BaseModel
+
+from models.enums import SubjectType, Days
+from shemas.homework import Homework
+
+
+class SubjectBase(BaseModel):
+    name: str
+    teacher: str
+    type: SubjectType
+    day: Days
+    time_start: time = None
+    time_end: time = None
+
+
+class SubjectCreate(SubjectBase):
+    pass
+
+
+class Subject(SubjectBase):
+    id: int
+    hws: list[Homework] = []
+
+    class Config:
+        orm_mode = True
