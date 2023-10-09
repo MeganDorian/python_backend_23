c4c7c45822cfdb7ee109b777f441d83162255f11
megan <julie.meh@yandex.ru>
2023-10-09 18:05:42 +0300

    feat: added some unit tests

--- tests/setup.py
+++ tests/setup.py
@@ -1,10 +1,13 @@
+import datetime
+
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker, Session
 
 from app.main import app
 from app.routers import get_db
 from db.database import Base
-from models.models import User
+from models.enums import SubjectType, Days
+from models.models import User, Subject, UserSubject
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 BASE_URL = "http://127.0.0.1:8000"
@@ -28,7 +31,13 @@ def test_db():
 def init_db(db: Session):
     user = User(name="Kelvin Kelly")
     db.add(user)
-
+    user = User(name="Milly Tompson")
+    db.add(user)
+    subject = Subject(name="Rodeo", teacher="Jelly Jam", type=SubjectType.LECTURE, day=Days.FRI,
+                      time_start=datetime.time(8, 0, 0), time_end=datetime.time(10, 0, 0))
+    db.add(subject)
+    user_sub = [UserSubject(id_user=1, id_subject=1), UserSubject(id_user=2, id_subject=1)]
+    db.add_all(user_sub)
     db.commit()
     db.close()
 
--- tests/unit.py
+++ tests/unit.py
@@ -1,6 +1,6 @@
 import pytest
 
-from crud.crud import get_users
+from crud.crud import get_users, get_user_subjects
 from tests.setup import TestSessionLocal
 
 
@@ -16,3 +16,11 @@ def test_get_users(setup_db):
     assert len(users) > 0
     assert type(users) is list
 
+
+@pytest.mark.parametrize("user_name, id, subject_name", [
+    ("Kelvin Kelly", 1, "Rodeo"),
+    ("Milly Tompson", 2, "Rodeo")])
+def test_user(user_name, id, subject_name, setup_db):
+    subjects = get_user_subjects(setup_db, id)
+    assert subjects is not None
+    assert subjects[0].name == subject_name
